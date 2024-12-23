38c9caf42634daf8c828a617cf879648a41e84eb
Stefano Verna <s.verna@datocms.com>
2024-12-23 09:21:44 +0100

    Add option -e

--- src/commands/files/decrypt.ts
+++ src/commands/files/decrypt.ts
@@ -1,5 +1,13 @@
 import { dirname, resolve } from 'node:path';
-import { boolean, command, flag, restPositionals } from 'cmd-ts';
+import {
+  boolean,
+  command,
+  flag,
+  option,
+  restPositionals,
+  string,
+} from 'cmd-ts';
+import { parse as parseDotEnv } from 'dotenv';
 import { ResolvedPath } from '../../utils/File';
 import { configFile } from '../../utils/configFile';
 import { decryptFile, decryptFileAndWrite } from '../../utils/encryption';
@@ -20,8 +28,15 @@ export default command({
       long: 'stdout',
       description: 'Should the decrypted content be written to stdout instead?',
     }),
+    envKey: option({
+      type: string,
+      short: 'e',
+      long: 'env-key',
+      description:
+        'Treats the file(s) as a dotenv file, looks for a specific env variable and prints it to stdout',
+    }),
   },
-  handler: async ({ explicitPaths, privateKey, toStdout }) => {
+  handler: async ({ explicitPaths, privateKey, toStdout, envKey }) => {
     const [config, _, configFilePath] = await configFile();
 
     const paths =
@@ -30,7 +45,20 @@ export default command({
         : config.files.map((path) => resolve(dirname(configFilePath), path));
 
     for (const file of paths) {
-      if (toStdout) {
+      if (envKey) {
+        const decryptedFile = await decryptFile(file, privateKey);
+
+        try {
+          const parsed = parseDotEnv(decryptedFile);
+
+          if (parsed[envKey]) {
+            console.log(parsed[envKey]);
+            break;
+          }
+        } catch (e) {
+          console.error(`Error parsing file ${file} as dotenv file:`, e);
+        }
+      } else if (toStdout) {
         console.log(await decryptFile(file, privateKey));
       } else {
         await decryptFileAndWrite(file, privateKey);
